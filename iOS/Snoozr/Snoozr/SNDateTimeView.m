92ca4daae299ce11126232019f95dbd09d624e1e
Devon Govett <devongovett@gmail.com>
2013-10-20 18:04:43 -0700

    Remove unused code

--- iOS/Snoozr/Snoozr/SNDateTimeView.m
+++ iOS/Snoozr/Snoozr/SNDateTimeView.m
@@ -92,13 +92,4 @@
     dateLabel.text = [formatter stringFromDate:date];
 }
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    // Drawing code
-}
-*/
-
 @end
