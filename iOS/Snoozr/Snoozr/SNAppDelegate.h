f259ed65c9e1ed3b5d832b459a482c4df3169c2c
Devon Govett <devongovett@gmail.com>
2013-10-27 14:51:07 -0700

    Clean up

--- iOS/Snoozr/Snoozr/SNAppDelegate.h
+++ iOS/Snoozr/Snoozr/SNAppDelegate.h
@@ -10,6 +10,4 @@
 
 @interface SNAppDelegate : UIResponder <UIApplicationDelegate>
 
-@property (strong, nonatomic) UIWindow *window;
-
 @end
--- iOS/Snoozr/Snoozr/SNBlurredViewController.m
+++ iOS/Snoozr/Snoozr/SNBlurredViewController.m
@@ -31,7 +31,7 @@
     // Dispose of any resources that can be recreated.
 }
 
--(UIStatusBarStyle)preferredStatusBarStyle
+- (UIStatusBarStyle)preferredStatusBarStyle
 {
     return UIStatusBarStyleLightContent;
 }
