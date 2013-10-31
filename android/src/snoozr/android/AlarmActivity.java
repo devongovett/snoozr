b919f540bafc5851ba72fa2781e9cf3a2554a14a
Donovan McKelvey <programmingmind@gmail.com>
2013-10-30 22:52:44 -0700

    use alarm stream

--- android/src/snoozr/android/AlarmActivity.java
+++ android/src/snoozr/android/AlarmActivity.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import android.app.Activity;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 
 public class AlarmActivity extends Activity{
@@ -12,11 +13,19 @@ public class AlarmActivity extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
-		
-        MediaPlayer mPlayer = MediaPlayer.create(this, R.raw.church_bells);
-        mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        
         try {
-			mPlayer.prepare();
+        	MediaPlayer mPlayer = new MediaPlayer();
+            mPlayer.setAudioStreamType(AudioManager.STREAM_ALARM);
+        	mPlayer.setDataSource(this, Uri.parse("android.resource://snoozr.android/" + R.raw.church_bells));
+        	mPlayer.prepare();
+            mPlayer.start();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} catch (IllegalStateException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -24,7 +33,5 @@ public class AlarmActivity extends Activity{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        
-        mPlayer.start();
 	}
 }
