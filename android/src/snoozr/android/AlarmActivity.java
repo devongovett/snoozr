d1d384ed61e0cc7988ef882011f50cfb76d57042
Donovan McKelvey <programmingmind@gmail.com>
2013-10-30 22:40:34 -0700

    play sound when activity starts

--- android/src/snoozr/android/AlarmActivity.java
+++ android/src/snoozr/android/AlarmActivity.java
@@ -1,6 +1,10 @@
 package snoozr.android;
 
+import java.io.IOException;
+
 import android.app.Activity;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class AlarmActivity extends Activity{
@@ -9,6 +13,18 @@ public class AlarmActivity extends Activity{
 		super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
 		
-		
+        MediaPlayer mPlayer = MediaPlayer.create(this, R.raw.church_bells);
+        mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        try {
+			mPlayer.prepare();
+		} catch (IllegalStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        mPlayer.start();
 	}
 }
