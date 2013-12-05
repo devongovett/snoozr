e8e760fa7cc52d29ae7abbc8b290c9e55d6d94c2
NiftyNico <nhiguera@calpoly.edu>
2013-12-04 23:11:25 -0700

    all but version

--- android/src/snoozr/android/SettingsFragment.java
+++ android/src/snoozr/android/SettingsFragment.java
@@ -2,23 +2,16 @@ package snoozr.android;
 
 
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceFragment;
-import android.app.Activity;
-import android.app.ListActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.graphics.drawable.Drawable;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
 
 public class SettingsFragment extends PreferenceFragment {
 	
@@ -26,7 +19,71 @@ public class SettingsFragment extends PreferenceFragment {
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.layout.activity_settings);
-        //getPreferences().edit().putBoolean(getResources().getString(id), arg1);
+        
+        Preference resetPreference = (Preference) getPreferenceScreen().findPreference("resetSchedule");
+        resetPreference.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+            public boolean onPreferenceClick(Preference preference) {
+            	
+                        
+                final Dialog dialog = new Dialog(getActivity());
+                dialog.setContentView(R.layout.simple_dialog);
+                dialog.setTitle("Confirmation");
+                //ImageView image = (ImageView) dialog.findViewById(R.drawable.adp_login_normal);
+                final TextView text = (TextView) dialog.findViewById(R.id.simple_dialot_text);
+                final Button cancel = (Button) dialog.findViewById(R.id.simple_dialog_cancel);
+                final Button confirm = (Button) dialog.findViewById(R.id.simple_dialog_confirm);
+                   
+                text.setText(getActivity().getResources().getString(R.string.confirmDelete));
+                    
+                cancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
+
+                confirm.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                    	
+                    	//TODO: delete neural network
+                        dialog.dismiss();
+                    }
+                });
+
+                dialog.show();
+                return true;
+            }
+        });
+        
+        Preference aboutPreference = (Preference) getPreferenceScreen().findPreference("about");
+        aboutPreference.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+            public boolean onPreferenceClick(Preference preference) {
+            	
+                        
+                final Dialog dialog = new Dialog(getActivity());
+                dialog.setContentView(R.layout.simple_dialog);
+                dialog.setTitle(getActivity().getResources().getString(R.string.about));
+                //ImageView image = (ImageView) dialog.findViewById(R.drawable.adp_login_normal);
+                final TextView text = (TextView) dialog.findViewById(R.id.simple_dialot_text);
+                ((Button) dialog.findViewById(R.id.simple_dialog_cancel)).setVisibility(View.GONE);
+                final Button confirm = (Button) dialog.findViewById(R.id.simple_dialog_confirm);
+                   
+                text.setText(getActivity().getResources().getString(R.string.authors));
+                confirm.setText(getActivity().getResources().getString(R.string.Ok));
+                
+                confirm.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                    	
+                        dialog.dismiss();
+                    }
+                });
+                
+                dialog.show();
+                return true;
+            }
+        });
     }
 
     public SharedPreferences getPreferences(){
